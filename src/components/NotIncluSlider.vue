714e9c86187f8ff6df9781b26727dacbf32444f9
sanxincao <lonelyparabola@gmail.com>
2023-06-25 16:27:21 +0800

    将页面使用静态数据抽出来，方便后续的数据处理 ```

--- src/components/NotIncluSlider.vue
+++ src/components/NotIncluSlider.vue
@@ -1,47 +1,9 @@
 <template>
     <div id="components-slider-demo-mark">
-      <h4>included=true</h4>
-      <a-slider v-model:value="value1" :marks="marks">
-        <template #mark="{ label, point }">
-          <template v-if="point === 100">
-            <strong>{{ label }}</strong>
-          </template>
-          <template v-else>{{ label }}</template>
-        </template>
-      </a-slider>
+      
       <a-slider v-model:value="value2" range :marks="marks">
         <template #mark="{ label, point }">
-          <template v-if="point === 100">
-            <strong>{{ label }}</strong>
-          </template>
-          <template v-else>{{ label }}</template>
-        </template>
-      </a-slider>
-  
-      <h4>included=false</h4>
-      <a-slider v-model:value="value3" :marks="marks" :included="false">
-        <template #mark="{ label, point }">
-          <template v-if="point === 100">
-            <strong>{{ label }}</strong>
-          </template>
-          <template v-else>{{ label }}</template>
-        </template>
-      </a-slider>
-  
-      <h4>marks & step</h4>
-      <a-slider v-model:value="value4" :marks="marks" :step="10">
-        <template #mark="{ label, point }">
-          <template v-if="point === 100">
-            <strong>{{ label }}</strong>
-          </template>
-          <template v-else>{{ label }}</template>
-        </template>
-      </a-slider>
-  
-      <h4>step=null</h4>
-      <a-slider v-model:value="value5" :marks="marks" :step="null">
-        <template #mark="{ label, point }">
-          <template v-if="point === 100">
+          <template v-if="point ===90">
             <strong>{{ label }}</strong>
           </template>
           <template v-else>{{ label }}</template>
@@ -51,33 +13,28 @@
   </template>
   <script lang="ts">
   import { defineComponent, ref } from 'vue';
+  import { Slider } from 'ant-design-vue';
+  import { products } from '@/store/data';
   import 'ant-design-vue/dist/antd.css'; 
   
   export default defineComponent({
+    components: {
+    'a-slider': Slider
+  },
     setup() {
-      const value1 = ref<number>(37);
-      const value2 = ref<[number, number]>([26, 37]);
-      const value3 = ref<number>(37);
-      const value4 = ref<number>(37);
-      const value5 = ref<number>(37);
+      const value2 = ref<[number, number]>([26, 37]); 
       const marks = ref<Record<number, any>>({
-        0: '0°C',
-        26: '26°C',
-        37: '37°C',
-        100: {
-          style: {
-            color: '#f50',
-          },
-          label: '100°C',
-        },
-      });
+        0: '0level',
+        26: '26level',
+        37: '37level',
+        54: '54level',
+        90: '90level',
+        },);
   
       return {
-        value1,
+        
         value2,
-        value3,
-        value4,
-        value5,
+       
         marks,
       };
     },
---
+++ src/store/data.ts
@@ -0,0 +1,24 @@
+export const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      description: "Description of Product 1",
+      price: 100,
+      image: "https://example.com/product1.jpg",
+      value2: [26, 37],
+      marks: {
+        0: '0level',
+        26: '26level',
+        37: '37level',
+        54: '54level',
+        90: '90level',
+      },
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      description: "Description of Product 2",
+      price: 200,
+      image: "https://example.com/product2.jpg",
+    },
+];
