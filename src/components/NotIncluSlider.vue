f1695da7f9d7a5131ff23d7fa56bbd7b81d8bfb2
sanxincao <lonelyparabola@gmail.com>
2023-07-03 16:26:51 +0800

    成功修改bug

--- src/components/NotIncluSlider.vue
+++ src/components/NotIncluSlider.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="components-slider-demo-mark">
       
-      <a-slider v-model:value="valueRef" :min="0" :max="90"  range :marks="mark" >
+      <a-slider v-model:value="valueRef" :min="0" :max="90"  range :marks="mark" @change="handleChange">
         <template #mark="{ label, point }">
           <template v-if="point ===90">
             <strong>{{ label }}</strong>
@@ -12,7 +12,8 @@
     </div>         
   </template>
   <script lang="ts">
-  import { defineComponent, ref,computed,getCurrentInstance,watch } from 'vue';
+  import { defineComponent, ref,computed,getCurrentInstance,watch,ComponentPublicInstance} from 'vue';
+  import Vue from "vue";
   import { Slider } from 'ant-design-vue';
   import { products } from '@/store/data';
   import 'ant-design-vue/dist/antd.css'; 
@@ -21,11 +22,14 @@
     components: {
     'a-slider': Slider
   },
-  
-  setup() {
+  props: {
+    ValueRange: Number,
+  },
+  emits: ['range-change:value'],
+  setup(props, { emit }) {
     
     const instance = getCurrentInstance();
-    const range = ref(1.0);
+    const range = ref(props.ValueRange);
     const redRef = computed(() => {
       const id = Number(instance?.proxy?.$route.params.id);
       const product = products.find((p) => p.id === id);
@@ -47,11 +51,20 @@
       range.value = max - min;
       
     });
-    
+    function handleChange() {
+      const min = Math.min(...valueRef.value);
+      const max = Math.max(...valueRef.value);
+      range.value = max - min;
+      console.log("emit:", range.value);
+      // 触发自定义事件
+      emit("range-change:value", range.value);
+    }
     return {
       redRef,
+      range,
       valueRef,
       mark,
+      handleChange,
     };
   },
   });
--- src/views/BuyPageView.vue
+++ src/views/BuyPageView.vue
@@ -3,19 +3,19 @@
       <h1>Buy {{ product.name }}</h1>
       <p>{{ product.description }}</p>
       <p>{{ product?.price }}</p>
-      <antcheckbox v-if="ShowCheckbox"/>
+      <antcheckbox v-if="ShowCheckbox" />
       <form @submit.prevent="buy">
-        <antslider v-if="showSlider"/>
+        <antslider v-if="showSlider" @range-change:value="handleSliderChange"/>
         
         <label for="quantity">Quantity:</label>
         <input type="number" id="quantity" name="quantity" v-model.number="quantity" required />
-        <button type="submit">Buy</button>
+        <button type="submit" >Buy</button>
       </form>
     </div>
   </template>
   
   <script lang="ts">
-  import { defineComponent, computed , ref,getCurrentInstance} from "vue";
+  import { defineComponent, computed , ref,getCurrentInstance,onMounted} from "vue";
   import antslider from "@/components/NotIncluSlider.vue";
   import antcheckbox from "@/components/CheckBox.vue";
   import { products } from '@/store/data';
@@ -42,6 +42,11 @@
       const producttemp = products.find((p) => p.id === id);
       return producttemp?.marks !== undefined;
     });
+    //获取子组件emit range-change:value携带的值
+    function handleSliderChange(value: number) {
+      console.log("handleSliderChange:", value);
+      Sumprice.value+=value;
+    }
 
     const ShowCheckbox = computed(() => {
       const id = Number(Currentinstance?.proxy?.$route.params.id);
@@ -62,6 +67,7 @@
       product,
       showSlider,
       ShowCheckbox,
+      handleSliderChange,
       buy,
     };
   },
