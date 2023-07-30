66ada148846603373c2799b122fdc7e54c2182be
sanxincao <lonelyparabola@gmail.com>
2023-07-30 22:25:18 +0800

    增加根组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <Header />
   <router-view />
+  <Footer />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Header from "@/components/MyAppheader.vue";
-
+import Footer from "@/components/Footer.vue";
 export default defineComponent({
   components: {
     Header,
+    Footer,
   },
 });
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="footer">
+      <div class="links">
+        <a href="#">相关链接1</a>
+        <a href="#">相关链接2</a>
+        <a href="#">相关链接3</a>
+      </div>
+      <div class="disclaimer">
+        <p>本站域名：example.com</p>
+        <p>本站符合审核声明</p>
+      </div>
+    </div>
+  </template>
+  
+  <style>
+  .footer {
+    background-color: #f5f5f5;
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  .links {
+    display: flex;
+  }
+  
+  .links a {
+    margin-right: 16px;
+    color: #333;
+    text-decoration: none;
+  }
+  
+  .disclaimer {
+    font-size: 14px;
+    color: #999;
+  }
+  </style>
